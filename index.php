8e478ce58f82d6d4d31f54edb2fc34431d4676a6
AL-FAHAMI TOIHIR <altoihir@gmail.com>
2022-03-16 14:51:36 +0100

    removed-unused-script

--- index.php
+++
@@ -1,4 +0,0 @@
-<h3>Site en cours de construction</h3>
-<p>Merci de visiter nos projets déjà en cours: <a href="/acemnews/">ACEM NEWS Site</a> | <a href="https://cra-acem.tech/">Club de Recherche académiques</a>
- | <a href="/jsc-fes-2021/">Journée Scientifique Culturelle</a>
-</p>
