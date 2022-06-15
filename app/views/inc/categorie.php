1643cbe93917ee80802ff25b8c3acd52434880b1
AL-FAHAMI TOIHIR <altoihir@gmail.com>
2022-06-15 20:16:55 +0100

    categorie-smedia-section

---
+++ app/views/inc/categorie.php
@@ -0,0 +1,10 @@
+<article class="card bg-light">
+    <h3 class="article-heading text-capitalize"><a>Catégories</a></h3>
+    <ul class="list">
+        <li><a href="<?php echo URLROOT; ?>/posts/categorie/festivites"><i class="fa fa-chevron-right"></i> Festivités</a></li>
+        <li><a href="<?php echo URLROOT; ?>/posts/categorie/be"><i class="fa fa-chevron-right"></i> Bureau Exécutif</a></li>
+        <li><a href="<?php echo URLROOT; ?>/posts/categorie/Projet"><i class="fa fa-chevron-right"></i> Projets</a></li>
+        <li><a href="<?php echo URLROOT; ?>/posts/categorie/Nouveautes"><i class="fa fa-chevron-right"></i> Nouveautés</a></li>
+        <li><a href="<?php echo URLROOT; ?>/posts/categorie/Archives"><i class="fa fa-chevron-right"></i> Archives</a></li>
+    </ul>
+</article>
--- app/views/posts/editer.php
+++ app/views/posts/editer.php
@@ -67,35 +67,11 @@
             </article>
 
             <div class="sidebar">
-                <article class="card bg-light">
-                    <h3 class="article-heading text-capitalize"><a href="article.html">Catégories</a></h3>
-                    <ul class="list">
-                        <li><a href="#"><i class="fa fa-chevron-right"></i> Festivités</a></li>
-                        <li><a href="#"><i class="fa fa-chevron-right"></i> Bureau Exécutif</a></li>
-                        <li><a href="#"><i class="fa fa-chevron-right"></i> Innovations</a></li>
-                        <li><a href="#"><i class="fa fa-chevron-right"></i> Projets</a></li>
-                        <li><a href="#"><i class="fa fa-chevron-right"></i> Articles</a></li>
-                    </ul>
-                </article>
-
-                <article class="card bg-dark">
-                    <div class="category category-communiques">Reseaux Sociaux</div>
-                    <h3 class="article-heading"><a href="article.html">Nous suivre</a></h3>
-                    <p> <a href="#"><i class="fab fa-facebook"></i> facebook.com/acemfesofficiel</a></p>
-                    <p><a href="#"><i class="fab fa-youtube"></i> youtube.com/acemfesofficiel</a></p>
-                    <p><a href="#"><i class="fab fa-instagram"></i> instagram.com/acemfesofficiel</a></p>
-
-                    <h3 class="article-heading"><a href="article.html">Nous ecrire</a></h3>
-                    <p><a href="#"><i class="fas fa-globe-africa"></i> acemfes.com/nouscontacter</a></p>
-                </article>
-
-                <article class="card bg-light">
-                    <div class="category category-alaune">PUBLICITE</div>
-                    <h3 class="article-heading"><a href="article.html">Votre publicite ici</a></h3>
-                    <p>
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Consectetur ut fugiat officiis laborum architecto, labore natus eveniet eos ullam assumenda!
-                    </p>
-                </article>
+            <?php 
+                require APPROOT . '/views/inc/categorie.php'; 
+                require APPROOT . '/views/inc/smedia.php'; 
+            ?>
+            
             </div>
         </div>
     </div>
